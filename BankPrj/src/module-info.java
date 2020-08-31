762d721b1b3949cb61630152d897d62da80486e7
Alexandr <60840713+X-Val01@users.noreply.github.com>
2020-08-31 16:26:09 +0300

    Add files via upload

---
+++ BankPrj/src/module-info.java
@@ -0,0 +1,10 @@
+module BankPrj {
+	exports com.mybank.domain;
+	exports com.mybank.data;
+	exports com.mybank.report;
+	exports com.mybank.batch;
+	exports com.mybank.test;
+	exports com.mybank.gui;
+
+	requires java.desktop;
+}
